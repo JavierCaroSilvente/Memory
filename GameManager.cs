2c3946b887edde0b26868c5e668899abbf8b3caf
DESKTOP-K5CS4OJ\xavic <xavicsilvente@gmail.com>
2021-12-04 21:40:00 +0100

    Ejercicio 3

--- GameManager.cs
+++ GameManager.cs
@@ -5,34 +5,35 @@ using UnityEngine;
 public class GameManager : MonoBehaviour
 {
     public GameObject cartaPrefab;
-    public List<GameObject> listaCartas = new List<GameObject>();
+    private List<GameObject> listaCartas = new List<GameObject>();
+
+    public int filasX;
+    public int columnasY;
 
     // Start is called before the first frame update
     void Start()
     {
-        int posX = -6;
-        int posY = 3;
+        int initPosX = -6;
+        int initPosY = 2;
 
-        for (int i = 0; i < 10; i++)
+        int cardsAmount = filasX * columnasY;
+        int saltoDeFila = columnasY - 1;
+        int distanceBetweenCardsY = -4;
+
+        for (int i = 0; i < cardsAmount; i++)
         {
-
-            GameObject newCartaPrefab = Instantiate(cartaPrefab, new Vector3(posX, posY, 0), Quaternion.identity);
+            GameObject newCartaPrefab = Instantiate(cartaPrefab, new Vector3(initPosX, initPosY, 0), Quaternion.identity);
             listaCartas.Add(newCartaPrefab);
             newCartaPrefab.name = "card" + i;
-            
-            posX += 3;
 
-            if(i == 4)
+            initPosX += 3;
+
+            if(i == saltoDeFila)
             {
-                posX = -6;
-                posY = -2;
+                saltoDeFila += columnasY;
+                initPosX = -6;
+                initPosY += distanceBetweenCardsY;
             }
         }
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
     }
 }
