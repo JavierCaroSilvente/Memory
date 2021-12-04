d7fc33f47b0f3e275c95cfc00066adc1942da851
DESKTOP-K5CS4OJ\xavic <xavicsilvente@gmail.com>
2021-12-04 20:19:10 +0100

    Ejercicio1

---
+++ CardScript.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(SpriteRenderer))]
+public class CardScript : MonoBehaviour
+{
+    public Sprite spriteBack;
+
+    void Start()
+    {
+        gameObject.GetComponent<SpriteRenderer>().sprite = spriteBack;
+
+        gameObject.transform.position = new Vector3(-1, 3, 0);
+    }
+
+}
